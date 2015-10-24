8404abb79ac240c6c69e9c9c3cf9f85b0b10a1a3
Miguel <miguel.mailbox@gmail.com>
2015-10-24 18:39:28 +0200

    Add tests directory.

---
+++ test/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use H4D\Leveret\Application;
+use H4D\Leveret\Validation\Adapters\H4DConstraintAdapter;
+use H4D\Validator\Constraints\Enum;
+
+require_once(__DIR__.'/../../vendor/autoload.php');
+
+$app = new Application();
+$app->registerRoute('GET', '/hello/:(string)name')
+    ->addRequestConstraints('name', new H4DConstraintAdapter((new Enum())->setOptions(['paco', 'maria'])))
+    ->setAction(
+        function ($name) use ($app)
+        {
+            $isValid = $app->isValidRequest();
+            var_dump($isValid);
+            if (!$isValid)
+            {
+                throw new \Exception($app->getRequestConstraintsViolationMessagesAsString());
+            }
+            $app->getResponse()->setBody('Hello '.$name);
+        });
+$app->run();
