286d6a73b9158dc830e98812459ebd74d17c0d8d
Miguel <miguel.mailbox@gmail.com>
2015-10-24 19:30:41 +0200

    Update test with symfony constraints adapter usage.

--- test/public/index.php
+++ test/public/index.php
@@ -2,7 +2,11 @@
 
 use H4D\Leveret\Application;
 use H4D\Leveret\Validation\Adapters\H4DConstraintAdapter;
+use H4D\Leveret\Validation\Adapters\SymfonyConstraintAdapter;
 use H4D\Validator\Constraints\Enum;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Required;
 
 require_once(__DIR__.'/../../vendor/autoload.php');
 
@@ -19,4 +23,22 @@ $app->registerRoute('GET', '/hello/:(string)name')
             }
             $app->getResponse()->setBody('Hello '.$name);
         });
+
+$app->registerRoute('GET', '/bye/:(string)name')
+    ->addRequestConstraints('name', [new SymfonyConstraintAdapter(new Required()),
+                                     new SymfonyConstraintAdapter(new NotBlank()),
+                                     new SymfonyConstraintAdapter(new Length(array('min'=>3,
+                                                                                   'max'=>10)))])
+    ->setAction(
+        function ($name) use ($app)
+        {
+            $isValid = $app->isValidRequest();
+            if (!$isValid)
+            {
+                throw new \Exception($app->getRequestConstraintsViolationMessagesAsString());
+            }
+            $app->getResponse()->setBody('Hello '.$name);
+        });
+
+
 $app->run();
