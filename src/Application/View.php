b221b9fcdb158c380dc264840aa6bbfe65c913d8
Miguel <miguel.mailbox@gmail.com>
2016-01-13 22:15:05 +0100

    Add new method 'translate' (shortcut for translator usage).

--- src/Application/View.php
+++ src/Application/View.php
@@ -2,13 +2,31 @@
 
 namespace H4D\Leveret\Application;
 
+use H4D\I18n\NullTranslator;
+use H4D\I18n\TranslatorAwareTrait;
 use H4D\Template\TemplateTrait;
 
 class View
 {
 
     use TemplateTrait;
+    use TranslatorAwareTrait;
 
-    public function __construct() {}
+    public function __construct()
+    {
+        $this->translator = new NullTranslator();
+    }
+
+    /**
+     * This function supports extra params for var substition in $string.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public function translate($string)
+    {
+        return $this->translator->translate(func_get_args());
+    }
 
 }
