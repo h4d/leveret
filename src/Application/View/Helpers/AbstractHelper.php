067557f1015f440a04580227f6f6d625fa8b58db
Miguel <miguel.mailbox@gmail.com>
2016-11-20 11:51:40 +0100

    Preliminary abstract class for view helpers.

---
+++ src/Application/View/Helpers/AbstractHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace H4D\Leveret\Application\View\Helpers;
+
+
+abstract class AbstractHelper
+{
+    /**
+     * @return string
+     */
+    abstract public function getAlias();
+
+}
